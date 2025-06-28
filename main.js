ca75e4e6f79cda9119f2349dfd855ba45614fc0d
sumire-m <supriyamo700@gmail.com>
2025-06-28 22:11:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
 var swiper = new Swiper(".home", {
         spaceBetween: 30,
         centeredSlides: true,
